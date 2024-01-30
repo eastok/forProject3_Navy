91b5b0094ffd1d248904017c0ea31a38bcf3fe60
02 <sonyuyoung@naver.com>
2024-01-30 14:13:11 +0900

    라인차트 수정

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,15 +1,65 @@
 import { ResponsiveLine } from "@nivo/line";
 import { useTheme } from "@mui/material";
 import { tokens } from "../theme";
-import { mockLineData as data } from "../data/mockData";
 
 const LineChart = ({ isCustomLineColors = false, isDashboard = false }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
+  const data = [
+    { name: "종로구", childAccidents: 28, "Gradient Boosting": 23.6 },
+    { name: "중구", childAccidents: 19, "Gradient Boosting": 19.02 },
+    { name: "용산구", childAccidents: 36, "Gradient Boosting": 36.01 },
+    { name: "성동구", childAccidents: 38, "Gradient Boosting": 38.03 },
+    { name: "광진구", childAccidents: 72, "Gradient Boosting": 71.98 },
+    { name: "동대문구", childAccidents: 60, "Gradient Boosting": 60.02 },
+    { name: "중랑구", childAccidents: 95, "Gradient Boosting": 94.97 },
+    { name: "성북구", childAccidents: 82, "Gradient Boosting": 82.03 },
+    { name: "강북구", childAccidents: 59, "Gradient Boosting": 45.1 },
+    { name: "도봉구", childAccidents: 54, "Gradient Boosting": 54 },
+    { name: "노원구", childAccidents: 92, "Gradient Boosting": 92 },
+    { name: "은평구", childAccidents: 85, "Gradient Boosting": 109.78 },
+    { name: "서대문구", childAccidents: 44, "Gradient Boosting": 44 },
+    { name: "마포구", childAccidents: 57, "Gradient Boosting": 56.99 },
+    { name: "양천구", childAccidents: 114, "Gradient Boosting": 113.99 },
+    { name: "강서구", childAccidents: 99, "Gradient Boosting": 98.98 },
+    { name: "구로구", childAccidents: 94, "Gradient Boosting": 89.31 },
+    { name: "금천구", childAccidents: 43, "Gradient Boosting": 42.98 },
+    { name: "영등포구", childAccidents: 88, "Gradient Boosting": 88 },
+    { name: "동작구", childAccidents: 66, "Gradient Boosting": 65.97 },
+    { name: "관악구", childAccidents: 58, "Gradient Boosting": 58.04 },
+    { name: "서초구", childAccidents: 80, "Gradient Boosting": 80.02 },
+    { name: "강남구", childAccidents: 115, "Gradient Boosting": 115 },
+    { name: "송파구", childAccidents: 140, "Gradient Boosting": 95.65 },
+    { name: "강동구", childAccidents: 87, "Gradient Boosting": 86.99 },
+  ];
+  // 데이터를 형식에 맞게 가공
+  const formattedData = [
+    {
+      id: "Gradient Boosting",
+      data: data.map(({ name, "Gradient Boosting": gradientBoosting }) => ({
+        x: name,
+        y: gradientBoosting,
+        value: gradientBoosting,
+      })),
+    },
+    {
+      id: "childAccidents",
+      data: data.map(({ name, childAccidents }) => ({
+        x: name,
+        y: childAccidents,
+        value: childAccidents,
+      })),
+    },
+  ];
+
   return (
     <ResponsiveLine
-      data={data}
+      data={formattedData}
+      colors={(d) => {
+        // "Gradient Boosting"인 경우 빨간색, "childAccidents"인 경우 파란색
+        return d.id === "Gradient Boosting" ? "#6971fa" : "#4dceac";
+      }}
       theme={{
         axis: {
           domain: {
@@ -43,14 +93,13 @@ const LineChart = ({ isCustomLineColors = false, isDashboard = false }) => {
           },
         },
       }}
-      colors={isDashboard ? { datum: "color" } : { scheme: "nivo" }} // added
       margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
       xScale={{ type: "point" }}
       yScale={{
         type: "linear",
-        min: "auto",
+        min: 0,
         max: "auto",
-        stacked: true,
+        stacked: false,
         reverse: false,
       }}
       yFormat=" >-.2f"
@@ -61,18 +110,19 @@ const LineChart = ({ isCustomLineColors = false, isDashboard = false }) => {
         orient: "bottom",
         tickSize: 0,
         tickPadding: 5,
-        tickRotation: 0,
-        legend: isDashboard ? undefined : "transportation", // added
+        tickRotation: -45,
+        legend: isDashboard ? undefined : "지역구",
         legendOffset: 36,
         legendPosition: "middle",
+        tickTextColor: "blue",
       }}
       axisLeft={{
         orient: "left",
-        tickValues: 5, // added
+        tickValues: 5,
         tickSize: 3,
         tickPadding: 5,
         tickRotation: 0,
-        legend: isDashboard ? undefined : "count", // added
+        legend: isDashboard ? undefined : "사고 수",
         legendOffset: -40,
         legendPosition: "middle",
       }}
--- src/scenes/line/index.jsx
+++ src/scenes/line/index.jsx
@@ -5,7 +5,10 @@ import LineChart from "../../components/LineChart";
 const Line = () => {
   return (
     <Box m="20px">
-      <Header title="교통사고 예측" subtitle="Simple Line Chart" />
+      <Header
+        title="교통사고 예측"
+        subtitle="지역구별 어린이사고수와 예측 어린이 사고수"
+      />
       <Box height="75vh">
         <LineChart />
       </Box>
