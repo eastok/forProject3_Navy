87251318f68bb7da802f686905ba36539545077e
jurwon <wndnjs1487@gmail.com>
2024-01-30 13:43:28 +0900

    OECD국가별 사고수 추가

--- src/scenes/geography/index.jsx
+++ src/scenes/geography/index.jsx
@@ -271,43 +271,24 @@ const Geography = () => {
                 backgroundColor={colors.primary[400]}
               >
                 <Box
-                  mt="25px"
-                  p="0 30px"
+                  // mt="25px"
+                  // p="0 30px"
                   display="flex "
                   justifyContent="space-between"
                   alignItems="center"
                 >
-                  <Box>
-                    <Typography
-                      variant="h5"
-                      fontWeight="600"
-                      color={colors.grey[100]}
-                    >
-                      Revenue Generated
-                    </Typography>
-                    <Typography
-                      variant="h3"
-                      fontWeight="bold"
-                      color={colors.greenAccent[500]}
-                    >
-                      $59,342.32
-                    </Typography>
-                  </Box>
-                  <Box>
-                    <IconButton>
-                      <DownloadOutlinedIcon
-                        sx={{
-                          fontSize: "26px",
-                          color: colors.greenAccent[500],
-                        }}
-                      />
-                    </IconButton>
-                  </Box>
+
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  alignItems="center"
+                >
+                <img src="../../assets/OECD국가별.png" alt="Social Analysis" style={{ width: "100%" }} />
                 </Box>
 
-                <Box height="250px" m="-20px 0 0 0">
-                  <LineChart isDashboard={true} />
                 </Box>
+
+            
               </Box>
 
               {/* ROW 3:  뭐 원형 그래프? */}
